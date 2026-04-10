7660bd36f81c921c34b495087ba3203de350adc2
Rei_Javv <kjcaceres1@icloud.com>
2026-04-10 09:47:04 -0500

    Version 2

--- project1.c
+++ project1.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 int main(){
-float num1, num2, promedio;
+float n1, n2, prom;
 
 do{
 printf("Ingrese la primera nota: ");
-scanf("%f",&num1);
-}while (num1<0||num1>20);
+scanf("%f",&n1);
+}while (n1<0||n1>20);
 do{
 printf("Ingrese la segunda nota: ");
-scanf("%f",&num2);
-}while(num2<0||num2>20);
-promedio=(num1+num2)/2;
+scanf("%f",&n2);
+}while(n2<0||n2>20);
+prom=(n1+n2)/2;
 
-if (promedio <11){
-    printf("Su promedio es: %.2f, reprobo.\n",promedio);
-} else if (promedio < 14){
-    printf("Su nota es: %.2f,va a supletorio.\n", promedio );
+if (prom <11){
+    printf("Su promedio es: %.2f, reprobo.\n",prom);
+} else if (prom < 14){
+    printf("Su nota es: %.2f,va a supletorio.\n", prom);
     }
 else{
-    printf("Su promedio es: %.2f, aprobo.\n", promedio);
+    printf("Su promedio es: %.2f, aprobo.\n", prom);
 return 0;
 } }
